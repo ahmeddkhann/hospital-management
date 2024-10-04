bfb2e45fe3a715a2af35d6af6253d24e1080a996
ahmeddkhann <iamahmedkhan02@gmail.com>
2024-10-04 18:17:02 +0500

    landing home added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(20px)' },
+        },
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
